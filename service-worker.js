d8da55ed0b869923ca3f0b76c6b76105d533759e
裴洪泽 <1941816504@qq.com>
2021-04-23 15:49:08 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/alibaba/precache-manifest.01a25d9c64f2b29d5df2b8ce6628eb0c.js"
+  "./precache-manifest.2c299b0d0b50d2503f5d2fcff04c8e2a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/alibaba/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
