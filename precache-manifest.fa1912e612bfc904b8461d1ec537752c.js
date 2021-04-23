d7c7dfbbffa9bb919432cf891a78e2179178b1e5
裴洪泽 <1941816504@qq.com>
2021-04-23 15:34:42 +0800

    Updates

---
+++ precache-manifest.fa1912e612bfc904b8461d1ec537752c.js
@@ -0,0 +1,314 @@
+self.__precacheManifest = [
+  {
+    "revision": "2948657c56e5cd9a019a",
+    "url": "/kitchen/static/css/0.999f5143.chunk.css"
+  },
+  {
+    "revision": "2948657c56e5cd9a019a",
+    "url": "/kitchen/static/js/0.2948657c.chunk.js"
+  },
+  {
+    "revision": "25bd5bae4387832561c5",
+    "url": "/kitchen/static/js/1.25bd5bae.chunk.js"
+  },
+  {
+    "revision": "2f1f885e378751822e6a",
+    "url": "/kitchen/static/js/2.2f1f885e.chunk.js"
+  },
+  {
+    "revision": "a0dccdc519371e83a4a9",
+    "url": "/kitchen/static/js/3.a0dccdc5.chunk.js"
+  },
+  {
+    "revision": "b3fb12b68688f9ed0fbd",
+    "url": "/kitchen/static/css/4.6d0143f2.chunk.css"
+  },
+  {
+    "revision": "b3fb12b68688f9ed0fbd",
+    "url": "/kitchen/static/js/4.b3fb12b6.chunk.js"
+  },
+  {
+    "revision": "4e81b7d9a1d40b1cf532",
+    "url": "/kitchen/static/css/5.5b86a63c.chunk.css"
+  },
+  {
+    "revision": "4e81b7d9a1d40b1cf532",
+    "url": "/kitchen/static/js/5.4e81b7d9.chunk.js"
+  },
+  {
+    "revision": "aa627308d0b734695f05",
+    "url": "/kitchen/static/css/main.6db643bb.chunk.css"
+  },
+  {
+    "revision": "aa627308d0b734695f05",
+    "url": "/kitchen/static/js/main.aa627308.chunk.js"
+  },
+  {
+    "revision": "e744dc574566ed06a14f",
+    "url": "/kitchen/static/js/runtime~main.e744dc57.js"
+  },
+  {
+    "revision": "b43c8d4b40b273d4055d",
+    "url": "/kitchen/static/css/8.019efc99.chunk.css"
+  },
+  {
+    "revision": "b43c8d4b40b273d4055d",
+    "url": "/kitchen/static/js/8.b43c8d4b.chunk.js"
+  },
+  {
+    "revision": "f52657552fa5f261285b",
+    "url": "/kitchen/static/css/9.270d82b1.chunk.css"
+  },
+  {
+    "revision": "f52657552fa5f261285b",
+    "url": "/kitchen/static/js/9.f5265755.chunk.js"
+  },
+  {
+    "revision": "d64c08568c3a05888c2c",
+    "url": "/kitchen/static/css/10.411c5e11.chunk.css"
+  },
+  {
+    "revision": "d64c08568c3a05888c2c",
+    "url": "/kitchen/static/js/10.d64c0856.chunk.js"
+  },
+  {
+    "revision": "02638d2c6a16947ba92e",
+    "url": "/kitchen/static/css/11.112db08f.chunk.css"
+  },
+  {
+    "revision": "02638d2c6a16947ba92e",
+    "url": "/kitchen/static/js/11.02638d2c.chunk.js"
+  },
+  {
+    "revision": "4ba0ce38511f638baa1c",
+    "url": "/kitchen/static/css/12.d91b509a.chunk.css"
+  },
+  {
+    "revision": "4ba0ce38511f638baa1c",
+    "url": "/kitchen/static/js/12.4ba0ce38.chunk.js"
+  },
+  {
+    "revision": "54aaa20dcf46633c7d8f",
+    "url": "/kitchen/static/css/13.9d4d0d4f.chunk.css"
+  },
+  {
+    "revision": "54aaa20dcf46633c7d8f",
+    "url": "/kitchen/static/js/13.54aaa20d.chunk.js"
+  },
+  {
+    "revision": "2e7328fe7d926cdcbc40",
+    "url": "/kitchen/static/css/14.4942bbf0.chunk.css"
+  },
+  {
+    "revision": "2e7328fe7d926cdcbc40",
+    "url": "/kitchen/static/js/14.2e7328fe.chunk.js"
+  },
+  {
+    "revision": "39d87d14d751051d0578",
+    "url": "/kitchen/static/css/15.2566d8b5.chunk.css"
+  },
+  {
+    "revision": "39d87d14d751051d0578",
+    "url": "/kitchen/static/js/15.39d87d14.chunk.js"
+  },
+  {
+    "revision": "1f1c7c0a9b1499376187",
+    "url": "/kitchen/static/css/16.1b92d8a4.chunk.css"
+  },
+  {
+    "revision": "1f1c7c0a9b1499376187",
+    "url": "/kitchen/static/js/16.1f1c7c0a.chunk.js"
+  },
+  {
+    "revision": "fa3f0a2ed6797a3e327d",
+    "url": "/kitchen/static/css/17.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "fa3f0a2ed6797a3e327d",
+    "url": "/kitchen/static/js/17.fa3f0a2e.chunk.js"
+  },
+  {
+    "revision": "33f54a2a24426ed37164",
+    "url": "/kitchen/static/css/18.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "33f54a2a24426ed37164",
+    "url": "/kitchen/static/js/18.33f54a2a.chunk.js"
+  },
+  {
+    "revision": "fa0392045ef8df557e3b",
+    "url": "/kitchen/static/css/19.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "fa0392045ef8df557e3b",
+    "url": "/kitchen/static/js/19.fa039204.chunk.js"
+  },
+  {
+    "revision": "f724b4c84224aee3e358",
+    "url": "/kitchen/static/css/20.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "f724b4c84224aee3e358",
+    "url": "/kitchen/static/js/20.f724b4c8.chunk.js"
+  },
+  {
+    "revision": "dec103bd5e3a97a258ac",
+    "url": "/kitchen/static/css/21.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "dec103bd5e3a97a258ac",
+    "url": "/kitchen/static/js/21.dec103bd.chunk.js"
+  },
+  {
+    "revision": "d6153e282e1f1e0b3961",
+    "url": "/kitchen/static/css/22.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "d6153e282e1f1e0b3961",
+    "url": "/kitchen/static/js/22.d6153e28.chunk.js"
+  },
+  {
+    "revision": "64198b3321c16ee72e02",
+    "url": "/kitchen/static/css/23.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "64198b3321c16ee72e02",
+    "url": "/kitchen/static/js/23.64198b33.chunk.js"
+  },
+  {
+    "revision": "020e5f3617a658c1f030",
+    "url": "/kitchen/static/css/24.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "020e5f3617a658c1f030",
+    "url": "/kitchen/static/js/24.020e5f36.chunk.js"
+  },
+  {
+    "revision": "c0bf37d95fa113c5215c",
+    "url": "/kitchen/static/css/25.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "c0bf37d95fa113c5215c",
+    "url": "/kitchen/static/js/25.c0bf37d9.chunk.js"
+  },
+  {
+    "revision": "f8fbac5cee285df97906",
+    "url": "/kitchen/static/css/26.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "f8fbac5cee285df97906",
+    "url": "/kitchen/static/js/26.f8fbac5c.chunk.js"
+  },
+  {
+    "revision": "5664fd185b21a787c2b1",
+    "url": "/kitchen/static/css/27.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "5664fd185b21a787c2b1",
+    "url": "/kitchen/static/js/27.5664fd18.chunk.js"
+  },
+  {
+    "revision": "6e773136bbb0dd56779c",
+    "url": "/kitchen/static/css/28.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "6e773136bbb0dd56779c",
+    "url": "/kitchen/static/js/28.6e773136.chunk.js"
+  },
+  {
+    "revision": "8b471d928f1530d1c8cb",
+    "url": "/kitchen/static/css/29.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "8b471d928f1530d1c8cb",
+    "url": "/kitchen/static/js/29.8b471d92.chunk.js"
+  },
+  {
+    "revision": "af3e63095b65bd982d99",
+    "url": "/kitchen/static/css/30.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "af3e63095b65bd982d99",
+    "url": "/kitchen/static/js/30.af3e6309.chunk.js"
+  },
+  {
+    "revision": "b6354f92d732eef59cb1",
+    "url": "/kitchen/static/css/31.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "b6354f92d732eef59cb1",
+    "url": "/kitchen/static/js/31.b6354f92.chunk.js"
+  },
+  {
+    "revision": "8adeb5378013699df7e0",
+    "url": "/kitchen/static/css/32.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "8adeb5378013699df7e0",
+    "url": "/kitchen/static/js/32.8adeb537.chunk.js"
+  },
+  {
+    "revision": "80fd330f93f2b3508524",
+    "url": "/kitchen/static/css/33.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "80fd330f93f2b3508524",
+    "url": "/kitchen/static/js/33.80fd330f.chunk.js"
+  },
+  {
+    "revision": "3106f65555ef2f0d95d7",
+    "url": "/kitchen/static/css/34.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "3106f65555ef2f0d95d7",
+    "url": "/kitchen/static/js/34.3106f655.chunk.js"
+  },
+  {
+    "revision": "e32405fce4408dc7fc02",
+    "url": "/kitchen/static/css/35.ab5531d7.chunk.css"
+  },
+  {
+    "revision": "e32405fce4408dc7fc02",
+    "url": "/kitchen/static/js/35.e32405fc.chunk.js"
+  },
+  {
+    "revision": "a6f386aeb3fc5f5d9e29",
+    "url": "/kitchen/static/js/36.a6f386ae.chunk.js"
+  },
+  {
+    "revision": "74b637ccd9874860ee3c",
+    "url": "/kitchen/static/css/37.92409588.chunk.css"
+  },
+  {
+    "revision": "74b637ccd9874860ee3c",
+    "url": "/kitchen/static/js/37.74b637cc.chunk.js"
+  },
+  {
+    "revision": "d6d0da5c1edb45f66c8d",
+    "url": "/kitchen/static/css/38.0cc66781.chunk.css"
+  },
+  {
+    "revision": "d6d0da5c1edb45f66c8d",
+    "url": "/kitchen/static/js/38.d6d0da5c.chunk.js"
+  },
+  {
+    "revision": "772dbb0a983b16272d80",
+    "url": "/kitchen/static/css/39.cbee8568.chunk.css"
+  },
+  {
+    "revision": "772dbb0a983b16272d80",
+    "url": "/kitchen/static/js/39.772dbb0a.chunk.js"
+  },
+  {
+    "revision": "30819e7d4de800a7b213",
+    "url": "/kitchen/static/js/40.30819e7d.chunk.js"
+  },
+  {
+    "revision": "27e60a39550d29b16894",
+    "url": "/kitchen/static/js/41.27e60a39.chunk.js"
+  },
+  {
+    "revision": "81fb39560f14ebb0ebdd04caaed85f80",
+    "url": "/kitchen/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2c299b0d0b50d2503f5d2fcff04c8e2a.js"
+  "/kitchen/precache-manifest.fa1912e612bfc904b8461d1ec537752c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/kitchen/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
